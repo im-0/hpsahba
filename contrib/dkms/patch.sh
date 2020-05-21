f08843512d9d41e677d811efe0b54ada32ba72ce
Arti Zirk <arti.zirk@gmail.com>
2020-05-21 21:25:27 +0300

    Add DKMS package with patched hpsa driver

---
+++ contrib/dkms/patch.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+VERSION=${1:-5.4}
+
+echo "Patching for kernel ${VERSION}"
+
+wget "https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/plain/drivers/scsi/hpsa.h?h=linux-${VERSION}.y" -O hpsa.h
+wget "https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/plain/drivers/scsi/hpsa.c?h=linux-${VERSION}.y" -O hpsa.c
+wget "https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/plain/drivers/scsi/hpsa_cmd.h?h=linux-${VERSION}.y" -O hpsa_cmd.h
+
+shopt -s nullglob
+for PATCH in ../../kernel/"${VERSION}"*/*.patch; do
+    echo "Applying ${PATCH}"
+    patch --no-backup-if-mismatch -Np3 < "${PATCH}"
+done
